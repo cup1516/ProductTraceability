7648460290b70fcc34238280dbb129c9bbd57932
smdh9264 <836591036@qq.com>
2020-06-21 14:19:44 +0800

    文件上传微服务

---
+++ pt/pt-profile/src/main/java/com/pt/ptprofile/PtProfileApplication.java
@@ -0,0 +1,13 @@
+package com.pt.ptprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtProfileApplication.class, args);
+    }
+
+}
---
+++ pt/pt-profile/src/test/java/com/pt/ptprofile/PtProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pt.ptprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
