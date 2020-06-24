1baca18fc1873e111af96d245bea0c53d7a7eb10
smdh9264 <836591036@qq.com>
2020-06-24 09:42:31 +0800

    修改资源服务器配置

--- pt/pt-user/src/main/java/com/pt/ptuser/PtUserApplication.java
+++ pt/pt-user/src/main/java/com/pt/ptuser/PtUserApplication.java
@@ -1,14 +1,14 @@
 package com.pt.ptuser;
 
 
+import com.pt.ptcommon.annotation.EnableCustomResourceServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.pt"})
+@EnableCustomResourceServer
 public class PtUserApplication {
 
     public static void main(String[] args) {
