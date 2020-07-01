1147c64c1c3061222c2eed9d31ff4591f45cb4db
1115485601@qq.com <1115485601@qq.com>
2020-07-01 14:13:03 +0800

    门户网站添加

---
+++ pt/pt-portal/src/main/java/com/pt/ptportal/dao/fileDao.java
@@ -0,0 +1,13 @@
+package com.pt.ptportal.dao;
+
+
+import com.pt.ptportal.entity.file;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface fileDao extends JpaRepository<file, Integer>, JpaSpecificationExecutor<file> {
+ Optional<file> findById(Integer id);
+
+}
